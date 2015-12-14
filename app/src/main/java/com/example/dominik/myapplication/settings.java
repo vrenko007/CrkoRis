e9a32ce1eeb26d4a0541b89f410f37433f427c6f
Haris <harly256@gmail.com>
2015-12-14 20:10:21 +0100

    settings menu

---
+++ app/src/main/java/com/example/dominik/myapplication/settings.java
@@ -0,0 +1,18 @@
+package com.example.dominik.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+public class settings extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+
+
+
+}
